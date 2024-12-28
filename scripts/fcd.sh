9cdae3ee823b3ede22942a134479137599cbf6a8
Petr Fusek <petr.fusek97@gmail.com>
2024-12-28 15:30:23 +0100

    feat: add verbose option to fcd script and update .gitignore to include .vscode/

--- scripts/fcd.sh
+++ scripts/fcd.sh
@@ -45,8 +45,9 @@ fcd() {
     local all=false
     local prt=false
     local help=false
+    local verbose=false
     local opts
-    opts=$(getopt -o hap --long help,all,print -n 'fcd' -- "$@")
+    opts=$(getopt -o hapv --long help,all,print,verbose -n 'fcd' -- "$@")
     
     if [ $? -ne 0 ]; then
         echo "ERROR: Failed to parse options." >&2
@@ -67,6 +68,10 @@ fcd() {
                 ;;
             -p|--print)
                 prt=true
+                shift
+                ;;
+            -v|--verbose)
+                verbose=true
                 shift
                 ;;
             --)
@@ -97,6 +102,7 @@ Changes the current directory to the specified directory or to a directory selec
     \033[1m-h\033[0m        Show this help message
     \033[1m-a\033[0m        Include hidden directories in the search
     \033[1m-p\033[0m        Print the selected directory instead of changing to it
+    \033[1m-v\033[0m        Verbose mode - print additional information
 
 \033[1mDirectory:\033[0m
     The directory to search for subdirectories in. Default is the current directory.
@@ -122,15 +128,22 @@ EOF
         local path=$(set -e && set -o pipefail && find_bfs -L "$dirName" -xdev 2>/dev/null | fzf)
     fi
     
+    # Clean the path: remove carriage returns and trailing/leading whitespace
+    path=$(echo "$path" | tr -d '\r' | xargs)
+    
+    [ "$verbose" = true ] && echo "Extracting directory from path: $path"
+    
     if test -f "$path"; then
         path=$(dirname "$path")
     fi
+    
+    [ "$verbose" = true ] && echo -e "\nWill navigate to $path"
     
     # Print the selected directory if -p option is specified
     if [ "$prt" == "true" ]; then
         echo "$path"
     else
-        # Change to the selected directory
-        cd "$path"
+        # Change to the selected directory using printf to handle special characters
+        cd "$(printf '%b' "${path}")"
     fi
 }
