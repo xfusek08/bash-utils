8d01d2a2cbe607abf231ae7acf8e6375e29e153f
Petr Fusek <petr.fusek97@gmail.com>
2024-12-30 17:19:09 +0100

    info about dependencies for copybuffer zdh feature.

--- zsh/features/copybuffer.zsh
+++ zsh/features/copybuffer.zsh
@@ -1,3 +1,7 @@
+# https://github.com/ohmyzsh/ohmyzsh/tree/d82669199b5d900b50fd06dd3518c277f0def869/plugins/copybuffer
+
+# needs sudo nala install wl-clipboard to work
+# https://askubuntu.com/a/1370797
 
 require_once "$ZSH_SCRIPTING_DIRECTORY/features/zinit.zsh"
 require_once "$ZSH_SCRIPTING_DIRECTORY/lib/clipboard.zsh"
