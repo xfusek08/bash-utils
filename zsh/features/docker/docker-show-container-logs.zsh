b1c769811805beacb4eba813cb4f8f7c3e13815f
Petr Fusek <petr.fusek97@gmail.com>
2025-01-22 10:14:06 +0100

    feat: update docker log viewing function to use batcat for improved output

--- zsh/features/docker/docker-show-container-logs.zsh
+++ zsh/features/docker/docker-show-container-logs.zsh
@@ -1,9 +1,8 @@
-require_once "$ZSH_SCRIPTING_DIRECTORY/features/bat.zsh"
 require_once "$ZSH_SCRIPTING_DIRECTORY/features/docker/_docker_completions.zsh"
 
 function docker-show-container-logs() {
     local container_id=$1
-    docker logs -f $container_id 2>&1 | bat --paging=never --plain --language=log
+    docker logs -f $container_id 2>&1 | stdbuf -oL batcat --paging=never --plain --language=log
 }
 
 alias dcl=docker-show-container-logs
