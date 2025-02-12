8a260bf14e7b3ecd1ca81971d3889fa7f4bde91e
Petr Fusek <petr.fusek97@gmail.com>
2025-02-12 09:09:40 +0100

    feat(zsh): watch-on-change better info for which files are being watched.

--- zsh/features/watch-for-change.zsh
+++ zsh/features/watch-for-change.zsh
@@ -1,5 +1,13 @@
 require_once "$SCRIPTS_PATH/ensure-entr.zsh"
 
+# From:
+# ".rw-rw-r--  289 petr 11 úno 10:28 -- /home/petr/sw/repo/work/xaver-5-monolith/bundles/xaver/assets/js/admin/themes/xaver/sulu-admin-bundle/components/Toolbar/toolbar.scss"
+# To:
+# ".rw-rw-r--  289 petr 11 úno 10:28 -- /h/p/s/r/w/x/b/x/a/j/a/t/x/s/c/Toolbar/toolbar.scss"
+reduce-path() {
+    sed -E 's|/([a-zA-Z0-9-]{4,})|/\1|g; s|/([a-zA-Z0-9-])([a-zA-Z0-9-]{3,})|/\1|g'
+}
+
 watch-on-change() {
     local files=()
     local all_files=""
@@ -26,6 +34,15 @@ watch-on-change() {
         echo "Error: No command specified after --"
         return 1
     fi
+
+    local to_watch_display=$(printf "$all_files" | tr '\n' '\0' | xargs -0 eza --long --git --color=always)
+    
+    echo "\nWatching these files:\n"
+    while IFS= read -r file; do
+        echo "$(echo "$file" | reduce-path)"
+    done <<< "$to_watch_display"
+    
+    echo "\nStarting watch...\n"
 
     printf "$all_files" | entr -ds "$*"
 }
